c7262c46f4f6d48d98fd8247149889c70348d46a
praneeth kanchanakuntla <69808321+Praneethkanchanakuntla@users.noreply.github.com>
2021-12-28 19:06:31 +0530

    Add files via upload

---
+++ hms/breast cancer/app.py
@@ -0,0 +1,26 @@
+import streamlit as st
+import pickle
+model=pickle.load(open('breast_cancer.pkl','rb'))
+def breast():
+    x=['radius_mean','texture_mean', 'perimeter_mean', 'area_mean',
+       'smoothness_mean', 'compactness_mean', 'concavity_mean',
+       'concave points_mean', 'symmetry_mean', 'fractal_dimension_mean',
+       'radius_se', 'texture_se', 'perimeter_se', 'area_se', 'smoothness_se',
+       'compactness_se', 'concavity_se', 'concave points_se', 'symmetry_se',
+       'fractal_dimension_se', 'radius_worst', 'texture_worst',
+       'perimeter_worst', 'area_worst', 'smoothness_worst',
+       'compactness_worst', 'concavity_worst', 'concave points_worst','symmetry_worst', 'fractal_dimension_worst']
+    lst=[]
+    for i in x:
+        x=st.number_input(i)
+        lst.append(x)
+    if st.button('predict'):
+        result=model.predict([lst])
+        if result==1:
+            st.title('you have')
+        else:
+            st.title('you dont')
+if __name__=="__main__":
+    breast()
+        
+         
